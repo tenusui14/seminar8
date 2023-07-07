e1e5279f42fb617c3da73d24f90a0a3790e822d9
Artem Tsytsylin <workhardgofock@gmail.com>
2023-07-07 22:25:10 +0300

    HW task60 is done

---
+++ HWtask60/Program.cs
@@ -0,0 +1,27 @@
+﻿// Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. Напишите программу, которая будет построчно выводить массив, добавляя 
+// индексы каждого элемента.
+// Массив размером 2 x 2 x 2
+// 66(0,0,0) 25(0,1,0)
+// 34(1,0,0) 41(1,1,0)
+// 27(0,0,1) 90(0,1,1)
+// 26(1,0,1) 55(1,1,1)
+
+int[,,] tripleMatrix = { { { 66, 25 }, { 34, 41 } }, { { 27, 90 }, { 26, 55 } } };
+
+void PrintTripleMatrix(int[,,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            for (int k = 0; k < matrix.GetLength(2); k++)
+            {
+                Console.Write($"{matrix[i, j, k]}({i},{j},{k})\t");
+            }
+            Console.WriteLine();
+        }
+
+    }
+}
+
+PrintTripleMatrix(tripleMatrix);
